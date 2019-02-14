8be5cc2803cb497332b653de1621a046b516f0de
John Wilson <johnner@me.com>
2019-02-14 14:58:43 -0800

    convert Spinner to material withStyles

--- imports/ui/components/Spinner/Spinner.js
+++ imports/ui/components/Spinner/Spinner.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-import './Spinner.scss';
+const styles = {
+  container: {
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: '0',
+    width: '100%',
+    height: '100%',
+    zIndex: '1000',
+    backgroundColor: 'rgba(255,255,255,0.66)',
+  },
+  icon: {
+    fontSize: '44px',
+  },
+};
 
-const Spinner = () => (
-  <div className="signin-spinner">
-    <div>
+const Spinner = ({ classes }) => (
+  <div className={classes.container}>
+    <div className={classes.icon}>
       <i className="fa fa-circle-o-notch fa-spin" />
     </div>
   </div>
 );
 
-export default Spinner;
+Spinner.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Spinner);
