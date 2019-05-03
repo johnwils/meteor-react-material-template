badea37d905b3ce62b11f8a7f412cf427a541116
John Wilson <johnner@me.com>
2019-05-03 13:48:35 -0700

    add constants

---
+++ imports/startup/client/lib/constants.js
@@ -0,0 +1,6 @@
+// business info
+export const companyName = 'company-name';
+export const companyAddress = 'company-address';
+export const supportEmail = 'support-email';
+export const websiteName = 'website-name';
+export const websiteUrl = 'website-url';
