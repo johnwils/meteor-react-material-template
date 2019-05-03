ec1e7c255fb255ce4f799efbbd95eb37a0632bcc
John Wilson <johnner@me.com>
2019-05-03 12:39:54 -0700

    update layout

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -18,36 +18,55 @@ import Profile from '../pages/Profile';
 import NotFound from '../pages/Not-Found';
 import RecoverPassword from '../pages/RecoverPassword';
 import ResetPassword from '../pages/ResetPassword';
+import TermsOfUse from '../pages/TermsOfUse';
+import PrivacyPolicy from '../pages/PrivacyPolicy';
 
 // import Spinner
 import Spinner from '../components/Spinner';
 
 // import hoc to pass additional props to routes
 import PropsRoute from '../pages/PropsRoute';
+import Footer from '../components/Footer/index';
 
 const App = props => (
   <Router>
-    <div>
+    <div
+      style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}
+    >
       <PropsRoute component={Navbar} {...props} />
-      {props.loggingIn && <Spinner />}
-      <Switch>
-        <PropsRoute exact path="/" component={Landing} {...props} />
-        <PropsRoute path="/signin" component={SignIn} {...props} />
-        <PropsRoute path="/signup" component={SignUp} {...props} />
-        <PropsRoute exact path="/profile" component={Profile} {...props} />
-        <PropsRoute exact path="/profile/:_id" component={Profile} {...props} />
-        <PropsRoute
-          path="/recover-password"
-          component={RecoverPassword}
-          {...props}
-        />
-        <PropsRoute
-          path="/reset-password/:token"
-          component={ResetPassword}
-          {...props}
-        />
-        <PropsRoute component={NotFound} {...props} />
-      </Switch>
+      <div style={{ flex: 1 }}>
+        {props.loggingIn && <Spinner />}
+        <Switch>
+          <PropsRoute exact path="/" component={Landing} {...props} />
+          <PropsRoute path="/signin" component={SignIn} {...props} />
+          <PropsRoute path="/signup" component={SignUp} {...props} />
+          <PropsRoute exact path="/profile" component={Profile} {...props} />
+          <PropsRoute
+            exact
+            path="/profile/:_id"
+            component={Profile}
+            {...props}
+          />
+          <PropsRoute
+            path="/recover-password"
+            component={RecoverPassword}
+            {...props}
+          />
+          <PropsRoute
+            path="/reset-password/:token"
+            component={ResetPassword}
+            {...props}
+          />
+          <PropsRoute path="/terms-of-use" component={TermsOfUse} {...props} />
+          <PropsRoute
+            path="/privacy-policy"
+            component={PrivacyPolicy}
+            {...props}
+          />
+          <PropsRoute component={NotFound} {...props} />
+        </Switch>
+      </div>
+      <PropsRoute component={Footer} {...props} />
     </div>
   </Router>
 );
