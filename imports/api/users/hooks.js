d072d8e4fd1b14d4f36cd4cdcf697b0eaf30770e
John Wilson <johnner@me.com>
2020-06-03 09:23:18 -0700

    fix admin role first user hook

--- imports/api/users/hooks.js
+++ imports/api/users/hooks.js
@@ -6,7 +6,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 
 Meteor.users.after.insert(function (userId, doc) {
-  if (!userId && Meteor.users.find().count() === 0) {
+  if (!userId && Meteor.users.find().count() === 1) {
     console.log("new admin registered, added to 'admin' role", doc._id);
     return Roles.addUsersToRoles(doc._id, ['admin'], Roles.GLOBAL_GROUP);
   }
