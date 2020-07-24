3ea8ad15bb5c619fd3649614113cf8a2ab79074b
John <johnner@me.com>
2020-07-24 13:11:42 -0700

    fix roles

--- imports/startup/server/roles.js
+++ imports/startup/server/roles.js
@@ -3,6 +3,5 @@ import { Meteor } from 'meteor/meteor';
 const roles = ['admin', 'user'];
 
 Meteor.startup(() => {
-  roles.forEach((role) =>
-    Roles.createRole(role, { unlessExists: true }));
+  roles.forEach((role) => Roles.createRole(role, { unlessExists: true }));
 });
