eea9254a4ca498b9376135d680433d4ccaf859f1
John <johnner@me.com>
2020-07-24 17:43:41 -0700

    fix cypress

--- imports/ui/components/Footer/tests/Footer.test.js
+++ imports/ui/components/Footer/tests/Footer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
-import Footer from './Footer';
+import Footer from '../Footer';
 
 describe('Footer Component', () => {
   test('component renders', () => {
